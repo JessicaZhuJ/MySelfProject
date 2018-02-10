9ae9a9233e0a927eb39d0a2eb7e15c883118b4f7
Jessica_Zhu <2577486408@qq.com>
2018-02-10 09:45:47 +0800

    [delete]删除原来所有内容

--- app/Myself/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="375ff623-ad6c-407d-a0a9-1c9bf16bd940")
-public class DataBindingInfo {}
